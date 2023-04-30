3c659dd59d32f8496b763e8b04ba5ff368a35f4a
Viktor <Viktor841995@gmail.com>
2023-04-30 23:29:18 +0300

    homework#6.1

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,5 @@ const categoriesList = document.querySelectorAll('.item')
 console.log(`Number of categories:${categoriesList.length}`)
 categoriesList.forEach(e => {
     console.log(`Category:${e.firstElementChild.textContent}`)
-    console.log(`Elements:${e.lastElementChild.querySelectorAll('li').length}`)
+    console.log(`Elements:${e.lastElementChild.children.length}`)
 })
